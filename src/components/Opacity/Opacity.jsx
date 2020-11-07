5000b722b5b6c209821b9e2ef327b1c6381447d6
TsapkoZh <60929704+TsapkoZh@users.noreply.github.com>
2020-11-07 18:01:00 +0300

    Opacity on GSAP

--- src/components/Opacity/Opacity.jsx
+++ src/components/Opacity/Opacity.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Tween, Timeline } from 'react-gsap';
-import { Controller, Scene } from 'react-scrollmagic';
+import { gsap } from 'gsap';
 
-const Opacity = ({ children, duration, isEnable }) => (
-  <Controller>
-    <Scene
-      enabled={isEnable}
-      indicators={false}
-      duration={duration}
-      triggerHook="onEnter"
-    >
-      <Timeline>
-        <Tween opacity="0">{children}</Tween>
-      </Timeline>
-    </Scene>
-  </Controller>
-);
+const Opacity = ({ children, id, className, trigger }) => {
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: `#${trigger}${id}`,
+        scrub: true,
+        end: 'top -5%',
+      },
+    });
+    tl.from(`#opacityEl${id}`, { opacity: 0.1 });
+  }, [id, trigger]);
+
+  return (
+    <div className={className} id={`opacityEl${id}`}>
+      {children}
+    </div>
+  );
+};
+
 Opacity.propTypes = {
-  children: PropTypes.object,
-  duration: PropTypes.string,
-  isEnable: PropTypes.bool,
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  id: PropTypes.string,
+  trigger: PropTypes.string,
 };
+
 Opacity.defaultProps = {
+  className: null,
   children: null,
-  duration: null,
-  isEnable: true,
+  id: null,
+  trigger: '',
 };
+
 export default React.memo(Opacity);
