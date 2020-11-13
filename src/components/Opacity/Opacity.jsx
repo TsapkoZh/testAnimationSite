6ebdc36ce97258a92e88f66dcd77021d279b04f7
TsapkoZh <60929704+TsapkoZh@users.noreply.github.com>
2020-11-13 12:31:55 +0300

    Parallax and opacity fix

--- src/components/Opacity/Opacity.jsx
+++ src/components/Opacity/Opacity.jsx
@@ -2,22 +2,22 @@ import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { gsap } from 'gsap';
 
-const Opacity = ({ children, id, className, trigger }) => {
-  const ref = useRef();
+const Opacity = ({ children, id, className }) => {
+  const opacityRef = useRef();
 
   useEffect(() => {
     const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: `#${trigger}${id}`,
+        trigger: `#${id}`,
         scrub: true,
         end: 'top -5%',
       },
     });
-    tl.from(ref.current, { opacity: 0.1 });
-  }, [id, trigger, ref]);
+    tl.from(opacityRef.current, { opacity: 0.1 });
+  }, [id, opacityRef]);
 
   return (
-    <div className={className} ref={ref}>
+    <div className={className} ref={opacityRef}>
       {children}
     </div>
   );
@@ -27,14 +27,12 @@ Opacity.propTypes = {
   className: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   id: PropTypes.string,
-  trigger: PropTypes.string,
 };
 
 Opacity.defaultProps = {
   className: null,
   children: null,
   id: null,
-  trigger: '',
 };
 
 export default React.memo(Opacity);
--- src/components/Parallax/Parallax.jsx
+++ src/components/Parallax/Parallax.jsx
@@ -2,19 +2,19 @@ import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { gsap } from 'gsap';
 
-const Parallax = ({ children, id, className, trigger }) => {
+const Parallax = ({ children, id, className }) => {
   const parallaxRef = useRef();
 
   useEffect(() => {
     const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: `#${trigger}${id}`,
+        trigger: `#${id}`,
         delay: 0.2,
         scrub: 0.1,
       },
     });
     tl.from(parallaxRef.current, { y: '-60%' });
-  }, [id, trigger, parallaxRef]);
+  }, [id, parallaxRef]);
 
   return (
     <div className={className} ref={parallaxRef}>
@@ -27,14 +27,12 @@ Parallax.propTypes = {
   className: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   id: PropTypes.string,
-  trigger: PropTypes.string,
 };
 
 Parallax.defaultProps = {
   className: null,
   children: null,
   id: null,
-  trigger: '',
 };
 
 export default React.memo(Parallax);
